ef60aca71a3f59ed6bac160e2b781d8e49f24d7b
alex-njaiya <ngangaalex795@gmail.com>
2026-08-14 15:27:16 +0300

    since I am using base62 to encode the orginal url then I get the nextval using the postgres sequence name and then use that Id to compute the hash/encoding

--- backend/internal/shortener/repository.go
+++ backend/internal/shortener/repository.go
@@ -2,6 +2,7 @@ package shortener
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -12,13 +13,12 @@ type PostgresRepository struct {
 
 type Repository interface {
 	GetShortCodeURL(ctx context.Context, shortCode string) (URL, error)
-	CreateShortCodeURL(ctx context.Context, OriginalURL string) (URL, error)
-	UpdateShortCodeURL(ctx context.Context, id int64, shortCode string) error
+	CreateShortCodeURL(ctx context.Context, id int64, shortCode string, OriginalURL string) error
+	ReserveID(ctx context.Context, id int64) (int64, error)
 }
- 
+
 // handles queries
 // 1. -- getting the shortcode of a url 2. entering/inserting a row of a new url 3. updating the row once the new code is computed
-
 
 func NewPosgresRepository(pool *pgxpool.Pool) *PostgresRepository {
 	return &PostgresRepository{
@@ -27,15 +27,56 @@ func NewPosgresRepository(pool *pgxpool.Pool) *PostgresRepository {
 }
 
 func (r *PostgresRepository) GetShortCodeURL(ctx context.Context, shortCode string) (*URL, error) {
-	var url URL
-	err := r.pool.QueryRow(ctx, 
-		`SELECT * FROM urls WHERE short_code = $1`, 
+	url := new(URL)
+	err := r.pool.QueryRow(ctx,
+		`SELECT * FROM urls WHERE short_code = $1`,
 		shortCode,
 	).Scan(&url.Id, &url.ShortCode, &url.OriginalURL, &url.CreatedAt)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("fetching url by code: %w", err)
+	}
+	return url, err
+}
+
+func (r *PostgresRepository) CreateShortCodeURL(ctx context.Context, id int64, shortCode string, originalURL string) error {
+	// instead of using the conventional design of transaction rollbacks we will use the sequence to the get the int
+	// for the next row in the table
+
+	_, err := r.pool.Exec(ctx,
+		`INSERT INTO urls (id, short_code, original_url) VALUES ($1, $2, $3)`,
+		id, shortCode, originalURL,
+	)
+
+	if err != nil {
+		return fmt.Errorf("inserting url: %w", err)
 	}
 
-	return &url, err
-} 
+	return nil
+}
+
+func (r *PostgresRepository) ReserveID(ctx context.Context) (int64, error) {
+	var sequenceName string
+
+	err := r.pool.QueryRow(ctx,
+		`SELECT pg_get_serial_sequence($1, $2)`,
+		"urls", "id",
+	).Scan(&sequenceName)
+
+	if err != nil {
+		return 0, fmt.Errorf("looking up sequence name: %w", err)
+	}
+
+	var nextID int64
+
+	err = r.pool.QueryRow(ctx,
+		`SELECT nextval($1)`,
+		sequenceName,
+	).Scan(&nextID)
+
+	if err != nil {
+		return 0, fmt.Errorf("reserving next id: %w", err)
+	}
+
+	return nextID, nil
+}
