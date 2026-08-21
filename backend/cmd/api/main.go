b0f39aa497ee11bdc226bd49177604f4bdfe65a8
alex-njaiya <ngangaalex795@gmail.com>
2026-08-22 00:57:36 +0300

    updated the frontend_url to correct format

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -48,7 +48,7 @@ func main() {
 
 	authHandler := auth.NewHandler(authService, cfg.JWT_SECRET)
 
-	router := httpserver.NewRouter(cfg.FRONTENDURL, shortenerHandler, authHandler, analyticsHandler)
+	router := httpserver.NewRouter(cfg.FRONTEND_URL, shortenerHandler, authHandler, analyticsHandler)
 
 	log.Printf("listening on :%s", cfg.PORT)
 
